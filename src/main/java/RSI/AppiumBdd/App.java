b83ca482bffb9bbe6b664476c95e963cb6c12798
Carlos Silva <carlos.silva>
2020-02-10 22:38:51 -0300

    primeiro commit

---
+++ src/main/java/RSI/AppiumBdd/App.java
@@ -0,0 +1,13 @@
+package RSI.AppiumBdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
